3c6dc7bdd464c1c6ce2e559130a703fc8b5d0ee1
BrandoSaenzRojas <brandosaenzrojas@hotmail.com>
2025-10-27 00:28:21 -0500

    fix(usuario): encripta contraseña con BCrypt antes de guardar en base de datos

--- src/main/java/com/inmobiliaria/inmobiliaria_backend/service/UsuarioService.java
+++ src/main/java/com/inmobiliaria/inmobiliaria_backend/service/UsuarioService.java
@@ -4,6 +4,7 @@ import com.inmobiliaria.inmobiliaria_backend.dto.UsuarioDTO;
 import com.inmobiliaria.inmobiliaria_backend.mapper.UsuarioMapper;
 import com.inmobiliaria.inmobiliaria_backend.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,9 +16,12 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioMapper usuarioMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO crearUsuario(UsuarioDTO usuarioDTO) {
         var usuarioEntidad = usuarioMapper.toEntity(usuarioDTO);
+        String passwordEncriptada = passwordEncoder.encode(usuarioEntidad.getPassword());
+        usuarioEntidad.setPassword(passwordEncriptada);
         var usuarioGuardado = usuarioRepository.save(usuarioEntidad);
         return usuarioMapper.toDTO(usuarioGuardado);
     }
